68d73720b9989a062fd120dc6dfb82a344c3fb75
Md Shohag Ali Sarder <shohagsb@gmail.com>
2022-08-22 00:51:09 +0600

    feat: room added

--- app/src/main/java/me/shohag/appiniontestproject/photo_gallery/data/database/PhotoDatabase.kt
+++ app/src/main/java/me/shohag/appiniontestproject/photo_gallery/data/database/PhotoDatabase.kt
@@ -1,8 +1,30 @@
 package me.shohag.appiniontestproject.photo_gallery.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [PhotoEntity::class], version = 1, exportSchema = false)
-abstract class PhotoDatabase {
+abstract class PhotoDatabase : RoomDatabase() {
+    abstract val photoDatabaseDao: PhotoDatabaseDao
 
+    companion object {
+        @Volatile
+        private var INSTANCE: PhotoDatabase? = null
+        fun getInstance(context: Context): PhotoDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        PhotoDatabase::class.java,
+                        "photo_gallery_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                }
+                return instance
+            }
+        }
+    }
 }
--- app/src/main/java/me/shohag/appiniontestproject/photo_gallery/data/database/PhotoDatabaseDao.kt
+++ app/src/main/java/me/shohag/appiniontestproject/photo_gallery/data/database/PhotoDatabaseDao.kt
@@ -3,12 +3,13 @@ package me.shohag.appiniontestproject.photo_gallery.data.database
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface PhotoDatabaseDao {
-    @Insert
-    suspend  fun insert(photo: PhotoEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg photo: PhotoEntity)
 
     @Query("SELECT * FROM photo_gallery_table")
     suspend  fun getAllPhotos(): LiveData<List<PhotoEntity>>
